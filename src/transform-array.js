f217d4463c31b6e9539a9cede39511c0ec4f7bd3
Tatsiana Haranina <tanyagoryanina.dev@gmail.com>
2025-01-02 18:07:15 +0300

    feat: transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  const result = [];
+
+  for (let i = 0; i < arr.length; i += 1) {
+    switch (arr[i]) {
+      case '--discard-next':
+        i += 1;
+        break;
+      case '--discard-prev':
+        result.splice(result.length - 1, 1);
+        break;
+      case '--double-next':
+        result.push(arr[i + 1]);
+        break;
+      case '--double-prev':
+        result.push(arr[i - 1]);
+        break;
+      default:
+        result.push(arr[i]);
+        break;
+    }
+  }
+
+  return result;
 }
 
 module.exports = {
