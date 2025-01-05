db8df144963c993293aca38480327b970d9168f0
Tatsiana Haranina <tanyagoryanina.dev@gmail.com>
2025-01-06 01:30:17 +0300

    feat: carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,20 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string' || isNaN(sampleActivity) || sampleActivity <= 0) {
+    return false;
+  }
+
+  const num = parseFloat(sampleActivity);
+
+  if (num >= MODERN_ACTIVITY) {
+    return false;
+  }
+
+  const age = Math.log(MODERN_ACTIVITY / num) / (0.693 / HALF_LIFE_PERIOD);
+  
+  return Math.floor(age);
 }
 
 module.exports = {
